5e925b0ce2105453f7316898d7383b9fc85f0c20
Mourad Mahgoub <mouradmahgoub710@gmail.com>
2023-12-18 06:17:12 +0200

    vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,4 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// createApp(App).use(vu).use(router).mount('#app')
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+createApp(App).use(vuetify).use(router).mount('#app')
