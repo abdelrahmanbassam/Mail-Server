243c6537ae0d1fbd4f78dab25ad1a70160f464b9
elkarnk <youssef.alaa.ahmed.haredy@gmail.com>
2023-12-19 20:59:44 +0200

    navbar and sidebar

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import MailListView from '@/views/Mails/MailListView.vue'
 import MailView from '@/views/Mails/MailView.vue'
 import LandingPage from '@/views/LandingPage/LandingPageView.vue'
-
+import NavBar from '@/views/NavBar.vue'
 
 const routes = [
   {
@@ -10,6 +10,11 @@ const routes = [
     name: 'mail-list-view',
     component: MailListView
   },
+  // {
+  //   path: '/',
+  //   name: 'mail-navbar-view',
+  //   component: NavBar
+  // },
   {
     path: '/mail/:id',
     name: 'mail-view',
--- frontend/src/views/Mails/MailListView.vue
+++ frontend/src/views/Mails/MailListView.vue
@@ -1,16 +1,21 @@
 <template>
-    <div>
+    <div class="show">
         <SideBar/>
+        <NavBar/>
     </div>
 </template>
 
 <script>
 import SideBar from '@/components/SideBar.vue';
+import NavBar from '@/views/NavBar.vue';
 export default {
-    components: {SideBar, }
+    components: {SideBar, NavBar,}
 }
 </script>
 
 <style>
+.show{
+    display: flex;
+}
 
 </style>
